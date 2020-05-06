3b9428035cd806a17ef6b30cca8cce2b5ef0be27
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-06 19:56:41 +0530

    print even numbers between 23 and 57

---
+++ Eleven.java
@@ -0,0 +1,12 @@
+ /*Write a program to print even numbers between 23 and 57. Each number should be printed in a separate row.*/
+ 
+ class Eleven{
+	 public static void main(String args[]){
+		 System.out.println("Even numbers between 23 and 57 are:- ");
+		 for(int i=24;i<=57;i++){
+			 if(i%2==0){
+				 System.out.println(i);
+			 }
+		 }
+	 }
+ }
