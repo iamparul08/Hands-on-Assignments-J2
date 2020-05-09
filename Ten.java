960296ca9cd1cde4b4839a69e1b06ae2525551e7
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-09 12:46:16 +0530

    Update Ten.java

--- Ten.java
+++ Ten.java
@@ -1,5 +1,3 @@
-/*Write a program to print numbers from 1 to 10 in a single row with one tab space.*/
-
 class Ten{
 	public static void main(String args[]){
 		System.out.println("Displaying numbers from 1 to 10: ");
