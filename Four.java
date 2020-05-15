bcdc03dbcaece2173dcbd103c625bc4f65aeff07
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-15 18:49:53 +0530

    Update Four.java

--- Four.java
+++ Four.java
@@ -1,9 +1,3 @@
-/*Initialize two character variables in a program and display the characters in alphabetical order.
-
-Example1) if the first character is 's' and second character is 'e' then the output should be  e,s
-
-Example2) if the first character is 'a' and second character is 'e', then the output should be a,e*/
-
 import java.util.Scanner;
 
 public class Four
