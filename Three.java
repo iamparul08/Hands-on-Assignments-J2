91cc71495a2d246b68cd40c684bfbde6263ea326
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-16 12:19:40 +0530

    two strings seperated by comma

--- Three.java
+++ Three.java
@@ -1,13 +1,3 @@
-/* if the program has not received arguments then print "No Values", else print all the values in a single line separated by ,(comma)
-
-Example1) java Example
-O/P: No values
-
-Example2) java Example Mumbai Bangalore
-O/P: Mumbai,Bangalore
-
-[Hint: You can use length property of an array to check its length]
-If Statement	*/
 
  import java.util.Scanner;
  class Three {
